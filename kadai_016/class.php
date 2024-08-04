4a1f3fc670be1d4672761d3d9b05b2c6da302af3
TAKAO-001 <168104465+TAKAO-001@users.noreply.github.com>
2024-08-04 11:06:30 +0900

    Add files via upload

---
+++ kadai_016/class.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP_課題_016</title>
+</head>
+<body>
+    <p>
+        <?php
+        class Food {
+            private $name;
+            private $price;
+
+         public function __construct(string $name,int $price){
+            $this->name = $name;
+            $this->price = $price;
+            }
+         public function show_price(){
+            return $this->price;
+            }   
+        }
+         $food = new Food('potato',250);
+         print_r($food);
+         echo '<br>';
+
+        class Animal{
+            private $name;
+            Private $height;
+            private $weight;
+         
+         public function __construct(string $name,int $height,int $weight){
+            $this->name = $name;
+            $this->height = $height;
+            $this->weight = $weight;
+            } 
+         public function show_height(){
+            return $this->height;
+            } 
+        }
+         $animal = new Animal ('dog',60,5000);
+         print_r($animal);
+         echo '<br>';
+
+         echo $food->show_price().'<br>';
+         echo $animal->show_height().'<br>';
+        
+    ?>
+</p>
+</body>
+</html>
